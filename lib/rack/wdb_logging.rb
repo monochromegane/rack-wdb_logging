05e8c16c16918184e119781fb1c25f9c96783ef0
monochromegane <dev.kuro.obi@gmail.com>
2017-10-10 14:55:38 +0900

    Initial commit.

---
+++ lib/rack/wdb_logging.rb
@@ -0,0 +1,7 @@
+require "rack/wdb_logging/version"
+
+module Rack
+  module WdbLogging
+    # Your code goes here...
+  end
+end
---
+++ test/test_helper.rb
@@ -0,0 +1,4 @@
+$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
+require 'rack/wdb_logging'
+
+require 'minitest/autorun'
